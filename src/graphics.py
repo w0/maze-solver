d1591eff9dd8ec338a0a63519eb07dd578bb80f6
w0 <git@kassidy.dev>
2024-06-16 15:51:10 -0500

    draws random lines now

--- src/graphics.py
+++ src/graphics.py
@@ -23,3 +23,28 @@ class Window:
 
     def close(self):
         self.running = False
+    
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+        
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.__p1 = point1
+        self.__p2 = point2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.__p1.x,
+            self.__p1.y,
+            self.__p2.x,
+            self.__p2.y,
+            fill=fill_color,
+            width=2
+        )
+    
--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
-from window import Window
+from graphics import Window, Point, Line
+
+from random import randrange
 
 def main():
     
     win = Window(800,600)
+
+    count = 0
+    p_sets = 15
+
+    while count < p_sets:
+        p1 = Point(randrange(800), randrange(600))
+        p2 = Point(randrange(800), randrange(600))
+        win.draw_line(Line(p1, p2), "black")
+        count += 1
+    
     win.wait_for_close()
 
 main()
