d7fac996417222a43624db5ed6d2052bb08ed551
w0 <git@kassidy.dev>
2024-06-25 20:05:51 -0500

    working cell class

--- src/graphics.py
+++ src/graphics.py
@@ -48,3 +48,35 @@ class Line:
             width=2
         )
     
+class Cell:
+    def __init__(self, point1: Point, point2: Point, window: Window):
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.has_left_wall = True
+        self.has_right_wall = True
+
+        self.__x1 = point1.x
+        self.__y1 = point1.y
+
+        self.__x2 = point2.x
+        self.__y2 = point2.y
+
+        self.__win = window
+
+    def draw(self):
+        b_left = Point(self.__x1, self.__y2)
+        b_right = Point(self.__x2, self.__y2)
+        t_left = Point(self.__x1, self.__y1)
+        t_right = Point(self.__x2, self.__y1)
+
+        if self.has_bottom_wall:
+            self.__win.draw_line(Line(b_left, b_right), "red")
+
+        if self.has_left_wall:
+            self.__win.draw_line(Line(b_left, t_left), "red")
+        
+        if self.has_top_wall:
+            self.__win.draw_line(Line(t_left, t_right), "blue")
+        
+        if self.has_right_wall:
+            self.__win.draw_line(Line(t_right, b_right), "blue")
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from graphics import Window, Point, Line
+from graphics import Window, Point, Line, Cell
 
 from random import randrange
 
@@ -6,15 +6,12 @@ def main():
     
     win = Window(800,600)
 
-    count = 0
-    p_sets = 15
-
-    while count < p_sets:
-        p1 = Point(randrange(800), randrange(600))
-        p2 = Point(randrange(800), randrange(600))
-        win.draw_line(Line(p1, p2), "black")
-        count += 1
+    p2 = Point(400, 500)
+    p1 = Point(500, 400)
     
+    my_cell = Cell(p1, p2, win)
+    my_cell.draw()
+
     win.wait_for_close()
 
 main()
